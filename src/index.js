d618c8dab1c9c5644103e9a565f7a9c227bd056f
Nataliia Lyfar <lifarnatala@gmail.com>
2022-01-03 21:16:16 +0200

    finished

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import './sass/main.scss';
 import '../js/mobile-menu';
 import '../js/modal';
 // import '../js/animate';
+import $ from 'jquery';
+import 'slick-carousel';
+import 'slick-carousel/slick/slick.scss';
+import 'slick-carousel/slick/slick-theme.scss';
+
+// $('.gallery__list').slick();
+    
